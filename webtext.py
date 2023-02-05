b08d14064e27b7beebf4f3d28917e9eb6e3e6c56
ohsem kwon <dhtpa95@gmail.com>
2023-02-05 17:43:48 +0900

    first commit

---
+++ webtext.py
@@ -0,0 +1,16 @@
+import nltk, matplotlib
+from nltk.corpus import webtext
+#print(webtext.fileids())
+
+fileid = 'singles.txt'
+wbt_words = webtext.words(fileid)
+fdist = nltk.FreqDist(wbt_words)
+
+print("최대 발생 토근", fdist.max(),'수 : ', fdist[fdist.max()])
+
+print("말뭉치 내 총 고유 토큰 수 : ", fdist.N())
+
+print("말뭉치에서 가장 흔한 10개 단어는 다음과 같습니다.")
+print(fdist.most_common(10))
+
+fdist.plot(cumulative=True)
